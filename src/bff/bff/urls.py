c15a0560da8e5d7eaa5e27eec82260ff99574f3d
BFF Dev Server <wayne@tsai.co>
2012-09-29 12:24:19 +1000

    before refactoring

--- src/bff/bff/urls.py
+++ src/bff/bff/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'bff.views.home', name='home'),
-    # url(r'^bff/', include('bff.foo.urls')),
+    url(r'^$', 'bff.views.home', name='home'),
+    url(r'^', include('bff.vote.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- src/bff/bff/vote/views.py
+++ src/bff/bff/vote/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, Http404
+import urllib2 as url
+import json
+import re
+
+def index(request):
+	return HttpResponse("LOL PHP")
+	
