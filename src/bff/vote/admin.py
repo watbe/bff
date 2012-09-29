88f56ef1e3f64339caf8dd4044b03ee22fcd7c69
Tim Sergeant <tgsergeant@gmail.com>
2012-09-29 15:27:10 +1000

    Added search and filter to meal admin

--- src/bff/vote/admin.py
+++ src/bff/vote/admin.py
@@ -8,9 +8,15 @@ class MenuAdmin(admin.ModelAdmin):
     inlines = [
         InlineMealAdmin,
     ]
+    list_filter = ('date',)
+
+class MealAdmin(admin.ModelAdmin):
+
+    search_fields = ('name',)
+    list_filter = ('categories__name',)
 
 admin.site.register(Menu, MenuAdmin)
-admin.site.register(Meal)
+admin.site.register(Meal, MealAdmin)
 admin.site.register(Vote)
 admin.site.register(Category)
 admin.site.register(VoteEvent)
