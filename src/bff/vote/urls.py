585dbd105f7dd9d51a34b58154c66cc3cf6bfd24
Tim Sergeant <tgsergeant@gmail.com>
2012-10-08 14:40:58 +1100

    Added graphs to statistics page and statistics index.

--- src/bff/vote/urls.py
+++ src/bff/vote/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('bff.vote.views',
 	url(r'^$', 'login'),
 	url(r'^vote/$', 'vote', name='vote'),
-	url(r'^stats/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'stats', name='stats'),
+	url(r'^stats/(?P<year>\d{4})/(?P<month>\d+)/(?P<day>\d+)/$', 'stats', name='stats'),
+	url(r'^stats/$', 'stats_index', name='stats_index'),
+	url(r'^stats/page/(?P<page>\d+)/$', 'stats_index_page', name='stats_index_page'),
 )
--- src/bff/vote/views.py
+++ src/bff/vote/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
 import datetime
 import json
+from django.core.paginator import Paginator, EmptyPage
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseNotFound
 from django.template.context import RequestContext
 from bff.vote.forms import LoginForm, RatingFormSet, valid_room, already_voted
 from bff.vote.models import Menu, VoteEvent
@@ -57,10 +58,20 @@ def vote(request):
 
 	return render_to_response('vote.html', {'formset':formset}, context_instance = RequestContext(request))
 
+def menu_exists(date, delta_days):
+	"""
+	Returns True if there is a menu in the database
+	for the given day, plus (or minus) delta_days
+	"""
+	del_date = date + datetime.timedelta(days=delta_days)
+	return Menu.objects.filter(date=del_date).exists()
+
 
 def stats(request, year, month, day):
-	date = datetime.date(int(year), int(month), int(day))
-
+	try:
+		date = datetime.date(int(year), int(month), int(day))
+	except ValueError:
+		return HttpResponseNotFound("That is not a valid date")
 	menu = get_object_or_404(Menu, date=date)
 	results = []
 	total_pos = 0
@@ -91,7 +102,29 @@ def stats(request, year, month, day):
 			'neutral':total_neutral,
 			'negative':total_neg,
 			'total':total_pos + total_neutral + total_neg,
-		}
+		},
+		'date':date,
 	}
+	if(menu_exists(date, -1)):
+		res_dict['prev'] = date - datetime.timedelta(1)
+	if(menu_exists(date, 1)):
+		res_dict['next'] = date + datetime.timedelta(1)
 
 	return render_to_response('stats.html', res_dict, context_instance = RequestContext(request))
+
+
+def stats_index(request):
+	return stats_index_page(request, 1)
+
+
+def stats_index_page(request, page):
+	all_menus = Menu.objects.order_by('-date')
+	paginator = Paginator(all_menus, 30)
+	page_num = int(page)
+
+	try:
+		menus = paginator.page(page_num)
+	except EmptyPage:
+		menus = paginator.page(paginator.num_pages)
+
+	return render_to_response('stats_index.html', {'menus':menus}, context_instance = RequestContext(request))
